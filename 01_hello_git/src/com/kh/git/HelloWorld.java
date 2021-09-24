d9e54d76777967fe72c1fe697a8a72229ccd2bda
JPLEE1991 <whitesunny65@naver.com>
2021-09-24 11:08:23 +0900

    good luck 출력문 추가

--- 01_hello_git/src/com/kh/git/HelloWorld.java
+++ 01_hello_git/src/com/kh/git/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world~");
 		System.out.println("hello git~");
-		
+		System.out.println("Good luck~");
 		new Foo().sayFoo();
 	}
 
